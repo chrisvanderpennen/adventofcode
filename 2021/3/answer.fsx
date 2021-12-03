da1190cf7b3c0436e6b5593373648abf86655464
Chris van der Pennen <chris.vanderpennen@alcidion.com>
2021-12-03 16:17:29 +1030

    day 3 clean up

--- 2021/3/answer.fsx
+++ 2021/3/answer.fsx
@@ -2,14 +2,25 @@ open System
 open System.IO
 
 let lines = 
-    File.ReadAllLines "input.txt"
-    |> Array.map Array.ofSeq
+    File.ReadLines "input.txt"
+    |> Seq.map (fun s -> s.ToCharArray())
+    |> List.ofSeq
     
-let transpose = Array.transpose lines
+let getMostCommonBit i (lst: char[] list) =
+    let res =
+        lst
+        |> List.map (Array.item i)
+        |> List.countBy id
+        |> List.maxBy (fun (a,b) -> b, match a with | '0' -> 0 | '1' -> 1)
+    fst res
 
 let swapBit = function | '0' -> '1' | '1' -> '0'
 
-let mostCommon = transpose |> Array.map (Array.countBy id >> Array.maxBy snd >> fst)
+let mostCommon = 
+    [0..11]
+    |> List.map (fun i -> getMostCommonBit i lines)
+    |> Array.ofList
+
 let mostCommonString = String mostCommon
 let mostCommonNumber = Convert.ToUInt32(mostCommonString, 2)
 
@@ -19,25 +30,12 @@ let leastCommonNumber = Convert.ToUInt32(leastCommonString, 2)
 
 printfn "%A %A %A" mostCommonNumber leastCommonNumber (mostCommonNumber * leastCommonNumber)
 
-let initList = List.ofArray lines
-
-let getCO2Bit i (lst: char[] list) =
-    let res =
-        lst
-        |> List.map (Array.item i)
-        |> List.countBy id
-        |> List.minBy (fun (a,b) -> b, match a with | '0' -> 0 | '1' -> 1)
-    fst res
-
 let [o2], [co2] =
     [0..11]
     |> List.fold 
         (fun (o2, co2) i ->
-            (match o2 with | [h] -> o2 | _ -> List.filter (Array.item i >> (=) (getCO2Bit i o2 |> swapBit)) o2), (match co2 with | [h] -> co2 | _ -> List.filter (Array.item i >> (=) (getCO2Bit i co2)) co2)
-        ) (initList, initList)
-
-printfn "%A" o2
-printfn "%A" co2
+            (match o2 with | [h] -> o2 | _ -> List.filter (Array.item i >> (=) (getMostCommonBit i o2)) o2), (match co2 with | [h] -> co2 | _ -> List.filter (Array.item i >> (=) (getMostCommonBit i co2 |> swapBit)) co2)
+        ) (lines, lines)
 
 let o2number = Convert.ToUInt32((String o2), 2)
 let co2number = Convert.ToUInt32((String co2), 2)
