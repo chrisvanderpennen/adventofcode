24de454349a06555addae57766ce874d70093567
Chris van der Pennen <chris.vanderpennen@alcidion.com>
2021-12-03 14:00:07 +1030

    knew there was a way to apply <

--- 2021/1/answer.fsx
+++ 2021/1/answer.fsx
@@ -1,7 +1,9 @@
 open System.IO
 
-let lines = File.ReadAllLines "input.txt"
+let lines = 
+    File.ReadAllLines "input.txt"
+    |> Array.map int
 
-printfn "%A" (lines |> Seq.map int |> Seq.pairwise |> Seq.filter (fun elem -> snd elem > fst elem) |> Seq.length)
+printfn "%A" (lines |> Seq.pairwise |> Seq.filter ((<||) (<)) |> Seq.length)
 
-printfn "%A" (lines |> Seq.map int |> Seq.windowed 3 |> Seq.map (Array.reduce (+)) |> Seq.pairwise |> Seq.filter (fun elem -> snd elem > fst elem) |> Seq.length)
+printfn "%A" (lines |> Seq.windowed 3 |> Seq.map (Array.reduce (+)) |> Seq.pairwise |> Seq.filter ((<||) (<)) |> Seq.length)
