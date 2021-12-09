7e26c99c95ab5a0da97599702c2a41a1d71b47da
Chris van der Pennen <chris.vanderpennen@gmail.com>
2021-12-09 21:18:39 +1030

    add a hashset/valuetuple version for part 2 day 9

--- 2021/9/answer.fsx
+++ 2021/9/answer.fsx
@@ -5,6 +5,9 @@
 
 open System
 open BenchmarkDotNet.Attributes
+open System.Collections.Generic
+open System.Collections.Concurrent
+open System.Threading.Tasks
 
 let inline isMinimum maxX maxY (arr: byte[,]) x y elem =
     (x = 0    || arr[x-1, y] > elem) &&
@@ -18,6 +21,14 @@ let getAdjacent maxX maxY x y =
         if x < maxX then yield (x+1, y)
         if y > 0 then yield (x, y-1)
         if y < maxY then yield (x, y+1)
+    ]
+
+let getAdjacentList maxX maxY x y =
+    [
+        if x > 0 then yield struct(x-1, y)
+        if x < maxX then yield struct(x+1, y)
+        if y > 0 then yield struct(x, y-1)
+        if y < maxY then yield struct(x, y+1)
     ]
 
 let rec doSearch getAdjacent arr acc visited (stack: Set<int*int>) =
@@ -34,6 +45,25 @@ let rec doSearch getAdjacent arr acc visited (stack: Set<int*int>) =
 let floodSearch (arr: byte[,]) pos =
     let getAdjacent = getAdjacent ((arr.GetLength 0) - 1) ((arr.GetLength 1) - 1)
     doSearch getAdjacent arr 0 Set.empty (Set [pos])
+
+let rec doSearchHashSet getAdjacent arr acc (visited: HashSet<struct(int*int)>) (stack: HashSet<struct(int*int)>) =
+    match stack.Count with
+    | 0 -> acc
+    | _ ->
+        let pos = Seq.head stack
+        let adjacent =
+            StructTuple2.apply getAdjacent pos
+            |> Seq.filter (fun pos -> StructTuple2.apply (Array2D.get arr) pos <> 9uy)
+            |> HashSet
+        adjacent.ExceptWith visited
+        stack.UnionWith adjacent
+        stack.Remove pos
+        visited.Add pos
+        doSearchHashSet getAdjacent arr (acc + 1) visited stack
+
+let floodSearchHashSet (arr: byte[,]) pos =
+    let getAdjacent = getAdjacentList ((arr.GetLength 0) - 1) ((arr.GetLength 1) - 1)
+    doSearchHashSet getAdjacent arr 0 (HashSet()) (HashSet [pos])
 
 type Answer () =
     inherit BaseAnswer()
@@ -56,7 +86,6 @@ type Answer () =
         let mutable minima = []
         Array2D.iteri (fun x y e -> if isMinimum x y e then minima <- (1 + int e)::minima) arr
         List.sum minima
-
 
     [<Benchmark>]
     member public this.partTwo () =
@@ -86,10 +115,41 @@ type Answer () =
         
         productOfTop3basinSizes
 
+    [<Benchmark>]
+    member public this.partTwoHashSet () =
+        let input = this.loadBinary ()
+        let width = input.IndexOf ((byte)'\n')
+        let height = input.Length / (width + 1)
+        let arr = Array2D.zeroCreate<byte> width height
+
+        for c = 0 to height - 1 do
+            let stride = c * (width + 1)
+            for r = 0 to width - 1 do
+                let i = stride + r
+                arr[r,c] <- input[i] - (byte)'0'
+        
+        let isMinimum = isMinimum (width - 1) (height - 1) arr
+
+        // let mutable minima = ConcurrentBag ()
+        // Parallel.For(0, arr.GetLength 0, (fun x -> ignore <| Parallel.For(0, arr.GetLength 1, (fun y -> if isMinimum x y arr[x,y] then minima.Add struct(x,y)))))
+        let mutable minima = []
+        Array2D.iteri (fun x y e -> if isMinimum x y e then minima <- struct(x,y)::minima) arr
+
+        let productOfTop3basinSizes =
+            minima
+            |> Array.ofSeq
+            |> Array.Parallel.map (floodSearchHashSet arr)
+            |> Array.sortDescending
+            |> Array.take 3
+            |> Array.reduce (*)
+        
+        productOfTop3basinSizes
+
 let answer = Answer ()
 answer.setup ()
 printfn "%A" (answer.partOne ())
 printfn "%A" (answer.partTwo ())
+printfn "%A" (answer.partTwoHashSet ())
 
 #if !INTERACTIVE
 open BenchmarkDotNet.Running
